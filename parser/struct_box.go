22e788bc4f27e309dcd1ff7be413be7fd7a5b70b
Ian MacLeod <ian@nevir.net>
2020-03-24 09:12:31 -0700

    Fix the field order of the FBox struct

--- parser/struct_box.go
+++ parser/struct_box.go
@@ -1,15 +1,16 @@
 package parser
 
+// https://github.com/EpicGames/UnrealEngine/blob/4.22/Engine/Source/Runtime/Core/Public/Math/Box.h#L17
 type FBox struct {
-	IsValid uint8    `json:"is_valid"`
 	Min     *FVector `json:"min"`
 	Max     *FVector `json:"max"`
+	IsValid uint8    `json:"is_valid"`
 }
 
 func (parser *PakParser) ReadFBox() *FBox {
 	return &FBox{
-		IsValid: parser.Read(1)[0],
 		Min:     parser.ReadFVector(),
 		Max:     parser.ReadFVector(),
+		IsValid: parser.Read(1)[0],
 	}
 }
